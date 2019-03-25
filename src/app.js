ca6c0954d4d40a82c97afcd13cf2cfc1b38ace18
Khadim Sarr <khadimsarr@outlook.com>
2019-03-25 12:47:58 +0000

    correct transfer link

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set(express.static, path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index", {
